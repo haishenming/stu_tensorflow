94612b79bb91970508626b9e345f8da2e4822690
xiangyufan <xiangyufan@juwang.cn>
2018-01-03 17:06:43 +0800

    init

--- test/start.py
+++ test/start.py
@@ -3,15 +3,14 @@ import numpy as np
 
 # create data
 x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data*0.1 + 0.3
-
+y_data = x_data * 0.1 + 0.3
 
 Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 biases = tf.Variable(tf.zeros([1]))
 
-y = Weights*x_data + biases
+y = Weights * x_data + biases
 
-loss = tf.reduce_mean(tf.square(y-y_data))
+loss = tf.reduce_mean(tf.square(y - y_data))
 
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
@@ -20,7 +19,7 @@ train = optimizer.minimize(loss)
 init = tf.global_variables_initializer()  # 替换成这样就好
 
 sess = tf.Session()
-sess.run(init)          # Very important
+sess.run(init)  # Very important
 
 for step in range(201):
     sess.run(train)
